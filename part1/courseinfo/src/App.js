e2129f9eeb4f2eb093b6fc596ef99a7ae7afde24
Ali Imran Anjum <ali.skole@outlook.com>
2022-08-25 11:11:39 +0200

    1.2: course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,9 +4,23 @@ const Header = (props) =>{
   )
 }
 
+const Part =(props) =>{
+  return(
+    <div>
+     {props.part} with {props.exercises} hours
+    </div>
+  )
+}
+
 const Content = (props) =>{
   return(
-    <p>{props.part} with {props.exercises} hours</p>
+    <div>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  
+    
   )
 }
 
@@ -28,9 +42,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}/>
       <Total total={exercises1+exercises2+exercises3}></Total>
     </div>
   )
